097f2adb649399137ad67439ccb87b5d97ee4279
Aisulu <aisulutulepova4@gmail.com>
2024-11-25 10:58:49 +0500

    bagylay

---
+++ index.js
@@ -0,0 +1,64 @@
+// // Тапсырма 1:
+// let san1 = +prompt('1-ші Санды енгізіңіз:')
+// let san2 = +prompt('2-ші Санды енгізіңіз:')
+
+
+// function Calculater1(){
+//     return san1+san2
+// }
+// alert(san1+san2)
+
+// function Calculater2(){
+//     return(san1-san2)
+// }
+// alert(san1-san2)
+
+// function Calculater3(){
+//     return(san1*san2)
+// }
+// alert(san1*san2)
+
+// function Calculater4(){
+//     return(san1/san2)
+// }
+// alert(san1/san2)
+
+
+
+
+
+
+
+
+// Тапсырма 2
+let zat = +prompt('Заттың  бағасын жазыңыз:')
+let san = +prompt('Қанша зат алғыңыз келеді:')
+let Jenildik = 0
+baga = zat*san
+
+function Jenildik1(){
+    if (baga>5000)
+        Jenildik =0.01
+    
+    alert(baga - Jenildik);
+}
+
+// function Jenildik2(){
+//     if (baga>10000)
+//         Jenildik = 20%
+//      baga = baga*san 
+//      alert(baga);
+
+// }
+// function Jenildik3(){
+//     if (baga>20000)
+//     return baga = baga*san - 30%
+//     alert(baga);
+// }
+
+
+
+
+
+    
+        
